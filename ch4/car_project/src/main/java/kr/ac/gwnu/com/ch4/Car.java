8f785fddc6ee457c40107bbaf57093d3613d1f90
suptudy <74354757+suptudy@users.noreply.github.com>
2021-01-24 21:00:49 +0900

    Add files via upload

---
+++ ch4/car_project/src/main/java/kr/ac/gwnu/com/ch4/Car.java
@@ -0,0 +1,35 @@
+package kr.ac.gwnu.com.ch4;
+
+public class Car {
+    private String name;
+    private int speed;
+
+    public Car() {
+
+    }
+
+    public Car(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void start() {
+        this.speed = 20;
+    }
+
+    public Object getSpeed() {
+        return this.speed;
+    }
+
+    public void accelerate() {
+        this.speed = this.speed + 30;
+
+    }
+
+    public void stop() {
+        this.speed = 0;
+    }
+}
---
+++ ch5/calendar_project/src/main/java/kr/ac/gwnu/com/ch5/Calendar.java
@@ -0,0 +1,52 @@
+package kr.ac.gwnu.com.ch5;
+
+public class Calendar {
+    private String[] dayNames = { "일요일", "월요일", "화요일", "수요일", "목요일", "금요일", "토요일" };
+    private int[] monthDays = { 0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+
+    public Calendar() {
+
+    }
+
+    public String askDay(int year, int month, int day) {
+        int totalDays = 0;
+
+        totalDays = totalDays + calculateTotalDays(year, month, day);
+        return dayNames[totalDays % 7];
+    }
+
+    private boolean isLeap(int year) {
+        boolean result = false;
+
+        if (year % 400 == 0) {
+            result = true;
+        } else if (year % 100 == 0) {
+            result = false;
+        } else if (year % 4 == 0) {
+            result = true;
+        } else {
+            result = false;
+        }
+        return result;
+    }
+
+    private int calculateTotalDays(int year, int month, int day) {
+        int totalDays = 0;
+        for (int y = 1; y < year; y++) {
+            if (isLeap(y))
+                totalDays = totalDays + 366;
+            else
+                totalDays = totalDays + 365;
+        }
+
+        for (int m = 1; m < month; m++)
+            totalDays = totalDays + monthDays[m];
+
+        if (isLeap(year) && month >= 3)
+            totalDays = totalDays + 1;
+
+        totalDays = totalDays + day;
+        return totalDays;
+    }
+
+}
